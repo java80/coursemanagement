cb9a483d73fa1dcaeb9db3adbd71e2f90be5241f
java80 <iekedede@hotmail.com>
2021-05-11 09:15:32 -0400

    courses.jsx done

---
+++ client/src/screens/Courses/courses.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '../../components/Card/Card';
+import Layout from '../../Layouts/Layout';
+
+export default function Courses(props) {
+  const {allCourses } = props
+  return (
+    <Layout>
+      <div className="card-list">
+        {allCourses.map(course => (
+          <React.Fragment key={course.id}>
+            <Link to={`/courses/${course.id}`}>
+              <Card
+                title={course.name}
+                image={course.imageurl}
+                description={course.breed}
+                category={course.category}
+              />
+            </Link>
+          </React.Fragment>
+        ))}
+      </div>
+      <Link to="/courses/new"><button>New course</button></Link>
+    </Layout>
+  )
+}
+
