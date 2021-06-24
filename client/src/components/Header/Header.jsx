492c362046ef1dccff9b8d2e3fb89d11a1d1913d
java80 <iekedede@hotmail.com>
2021-06-24 14:48:45 -0400

    Redesigned header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,13 +3,16 @@ import { Link, NavLink } from "react-router-dom";
 import { logoutUser } from "../../services/auth";
 import { useHistory } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import AppBar from "@material-ui/core/AppBar";
 
 export default function Header(props) {
   const history = useHistory();
-
+  const authToken = localStorage.getItem("authToken");
+  console.log("Header props", props);
+  console.log("Auth Token from header", authToken);
   const handleLogout = () => {
     logoutUser();
+    // props.setauthToken("");
+    localStorage.removeItem("authToken");
     props.setCurrentUser(null);
     history.push("/");
   };
@@ -21,76 +24,66 @@ export default function Header(props) {
   });
 
   return (
-    
-      <header>
-        {/* <Link to='/home'>
+    <header>
+      {/* <Link to='/home'>
       </Link> */}
-        <div className="headerContainer">
-          <div className="innerHeader d-flex justify-content-between align-items-center">
-            <NavLink
-              className="text-decoration-none text-white fw-bold h4 mb-0 logo"
-              exact
-              to="/"
-            >
-              Teach App
-            </NavLink>
-            <GiHamburgerMenu
-              onClick={props.ClickEvent}
-              className="d-block d-md-none pointer"
-              color="#fff"
-              fontSize="1.5rem"
-            />
-            <ul className="d-none d-md-flex list-unstyled justify-content-center flex-wrap mb-0">
-              <li className="me-5">
-                <NavLink
-                  activeClassName="active"
-                  className="text-decoration-none"
-                  exact
-                  to="/"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="me-5">
-                <NavLink
-                  activeClassName="active"
-                  className="text-decoration-none"
-                  exact
-                  to="/signup"
-                >
-                  Teach Here
-                </NavLink>
-              </li>
-              <li className="me-5">
-                <NavLink
-                  activeClassName="active"
-                  className="text-decoration-none"
-                  exact
-                  to="/signup"
-                >
-                  Sign Up
-                </NavLink>
-              </li>
-              <li className="me-5">
-                <NavLink
-                  activeClassName="active"
-                  className="text-decoration-none"
-                  exact
-                  to="/login"
-                >
-                  Log In
-                </NavLink>
-              </li>
+      <div className="headerContainer">
+        <div className="innerHeader d-flex justify-content-between align-items-center">
+          <NavLink
+            className="text-decoration-none text-white fw-bold h4 mb-0 logo"
+            exact
+            to="/"
+          >
+            Teach App
+          </NavLink>
+          <GiHamburgerMenu
+            onClick={props.ClickEvent}
+            className="d-block d-md-none pointer"
+            color="#fff"
+            fontSize="1.5rem"
+          />
+          <ul className="d-none d-md-flex list-unstyled justify-content-center flex-wrap mb-0">
+            <li className="me-5">
+              <NavLink className="text-decoration-none" exact to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="me-5">
+              <NavLink className="text-decoration-none" exact to="/signup">
+                Teach Here
+              </NavLink>
+            </li>
+            {!authToken && (
+              <>
+                <li className="me-5">
+                  <NavLink className="text-decoration-none" exact to="/signup">
+                    Sign Up
+                  </NavLink>
+                </li>
+                <li className="me-5">
+                  <NavLink className="text-decoration-none" exact to="/login">
+                    Log In
+                  </NavLink>
+                </li>
+              </>
+            )}
+
+            {authToken && (
               <li>
-                <Link className="text-decoration-none" onClick={handleLogout}>
+                <Link
+                  to="/login"
+                  className="text-decoration-none"
+                  onClick={handleLogout}
+                >
                   logout
                 </Link>
               </li>
-              <li> {props.currentUser && props.currentUser.email}</li>
-            </ul>
-          </div>
+            )}
+
+            <li> {props.currentUser && props.currentUser.email}</li>
+          </ul>
         </div>
-      </header>
-   
+      </div>
+    </header>
   );
 }
